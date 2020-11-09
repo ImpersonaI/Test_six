313d3cfe121aa579f8f77b42d4a9b5ad8cc7d091
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-11-09 19:10:28 +0300

    Add files via upload

---
+++ Test_six/Test_six/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+
+namespace Test_six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"log_output.txt");
+            var new_in = new StreamReader(@"log_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Log log = new Log();
+            log.Input();
+            log.GetLog();
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+
+        }
+    }
+}
---
+++ Test_six/Test_six/log.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_six
+{
+    class Log
+    {
+        double a, b, log;
+        public double A { get; set; }
+        public double B { get; set; }
+
+
+        public void Input()
+        {
+            Console.WriteLine("Input a =");
+            A = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Input b =");
+            B = Convert.ToDouble(Console.ReadLine());
+            this.a = A;
+            this.b = B;
+        }
+    
+        public void GetLog()
+        {
+            log = 1 / 2 * a * a - 10 * a - 1 / 2 * b * b - 10 * b;
+            Console.WriteLine("a =" + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("log = "+ log);
+        }
+    }
+}
